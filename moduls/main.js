f0184abf048f02c0b268b5c26fe9adf146448758
Enmanuel Otero M <oteromontanoenmanuel@gmail.com>
2022-04-25 12:54:38 -0300

    href central america

--- moduls/main.js
+++ moduls/main.js
@@ -34,7 +34,7 @@ const saveCountriesInArray = async ()=> {
     const america = await callCountry()//Llamada a la función que hace la solicitud.
     if(location.href === `${location.protocol}//${location.host}/Juego-de-Banderas/pages/south-america/south-america.html`){
         currentRegion = america.filter(element => element.subregion == "South America")
-    }else if(location.pathname === `${location.protocol}//${location.host}/pages/central-north-america-caribbean/central-north-america-caribbean.html`){
+    }else if(location.pathname === `${location.protocol}//${location.host}/Juego-de-Banderas/pages/central-north-america-caribbean/central-north-america-caribbean.html`){
         const caribbean = america.filter(element => element.name.common == "Cuba" || element.name.common == "Dominican Republic" || element.name.common == "Haiti" || element.name.common == "Bahamas" || element.name.common == "Jamaica" || element.name.common == "Puerto Rico" || element.name.common == "Trinidad and Tobago")
         const centralAndNorthAmerica = america.filter(element => element.subregion == "Central America" || element.subregion == "North America"  && element.name.common !== "Saint Pierre and Miquelon" && element.name.common !== "United States Minor Outlying Islands")
         currentRegion = caribbean.concat(centralAndNorthAmerica)
@@ -125,7 +125,7 @@ const calculatePoints = (containerLeft, containerCenter, containerRight)=>{
 }
 
 nextRegion.addEventListener("click", ()=>{
-    location.href = `${location.protocol}//${location.host}/pages/central-north-america-caribbean/central-north-america-caribbean.html`
+    location.href = `${location.protocol}//${location.host}/Juego-de-Banderas/pages/central-north-america-caribbean/central-north-america-caribbean.html`
 })
 
 buttonNext.addEventListener("click", ()=> {
